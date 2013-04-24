70757fcdd12be4a4af34aeebda76f3672b3fa6bc
Sami Saada <saada@cs.helsinki.fi>
2013-04-24 19:08:35 +0300

    rampage mid when can

--- teams/default/teambot.lua
+++ teams/default/teambot.lua
@@ -3,7 +3,38 @@ local teambot = _G.object
 
 runfile 'bots/core_teambot.lua'
 
-teambot.myName = 'Template Team'
+local core = teambot.core
+
+teambot.myName = 'Default Team'
+
+local function IsGanker(unit)
+  return unit:GetTypeName() == "Hero_Rampage"
+end
+
+local function IsMidCarry(unit)
+  return unit:GetTypeName() == "Hero_Krixi"
+end
+
+function teambot.FindBestLaneSoloOverride(tAvailableHeroes)
+  if core.NumberElements(tAvailableHeroes) == 0 then
+    return nil, nil
+  end
+
+  local bGankerFound = false
+  local unitBestUnit = nil
+  for _, unit in pairs(tAvailableHeroes) do
+    if IsGanker(unit) then
+      bGankerFound = true
+      unitBestUnit = unit
+    elseif not bGankerFound and IsMidCarry(unit) then
+      unitBestUnit = unit
+    end
+  end
+
+  return unitBestUnit or teambot.FindBestLaneSoloOld(tAvailableHeroes)
+end
+teambot.FindBestLaneSoloOld = teambot.FindBestLaneSolo
+teambot.FindBestLaneSolo = teambot.FindBestLaneSoloOverride
 
 ------------------------------------------------------
 --            onthink override                      --
