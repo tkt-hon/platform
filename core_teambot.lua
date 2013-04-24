1090b514469c7c4d145d2a5a41eca1bbed28eb3f
Sami Saada <saada@cs.helsinki.fi>
2013-04-24 04:01:14 +0300

    teambot refactoring

--- core_teambot.lua
+++ core_teambot.lua
@@ -1,6 +1,4 @@
 local _G = getfenv(0)
 local teambot = _G.object
 
-function teambot.UseOriginal()
-  runfile 'bots/teambot/teambotbrain.lua'
-end
+runfile 'bots/teambot/teambotbrain.lua'
--- teams/template/teambot.lua
+++ teams/template/teambot.lua
@@ -1,8 +1,20 @@
 local _G = getfenv(0)
 local teambot = _G.object
 
-teambot.name = 'Template Team'
-
 runfile 'bots/core_teambot.lua'
 
-teambot.UseOriginal()
+teambot.myName = 'Template Team'
+
+------------------------------------------------------
+--            onthink override                      --
+-- Called every bot tick, custom onthink code here  --
+------------------------------------------------------
+-- @param: tGameVariables
+-- @return: none
+function teambot:onthinkOverride(tGameVariables)
+  self:onthinkOld(tGameVariables)
+
+  -- custom code here
+end
+teambot.onthinkOld = teambot.onthink
+teambot.onthink = teambot.onthinkOverride
