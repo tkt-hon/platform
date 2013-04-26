af073e0ad48a23d8e4364705e86685d5310adcb9
Sami Saada <saada@cs.helsinki.fi>
2013-04-26 03:46:13 +0300

    seed wards

--- teams/default/default_glacius.lua
+++ teams/default/default_glacius.lua
@@ -3,3 +3,16 @@ local glacius = _G.object
 
 runfile 'bots/glacius/glacius_main.lua'
 runfile 'bots/teams/default/utils/sitter.lua'
+
+local tinsert = _G.table.insert
+local core, behaviorLib = glacius.core, glacius.behaviorLib
+
+local function ShopUtilityOverride(botBrain)
+  local seeded = behaviorLib.canAccessShopLast
+  local utility = behaviorLib.ShopUtility(botBrain)
+  if seeded ~= behaviorLib.canAccessShopLast then
+    tinsert(behaviorLib.curItemList, 1, "Item_FlamingEye")
+  end
+  return utility
+end
+behaviorLib.ShopBehavior["Utility"] = ShopUtilityOverride
