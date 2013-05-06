f3894c3b464489af3317165a65220282500b4e6d
Jerry Mesimaki <jerry.mesimaki@cs.helsinki.fi>
2013-05-06 11:23:15 +0300

    eka commitpush

---
+++ teams/2easy/2easy_moon_queen.lua
@@ -0,0 +1,47 @@
+local _G = getfenv(0)
+local moonqueen = _G.object
+
+moonqueen.heroName = "Hero_Krixi"
+
+runfile 'bots/core_herobot.lua'
+
+---------------------------------------------------------------
+--            SkillBuild override                            --
+-- Handles hero skill building. To customize just write own  --
+---------------------------------------------------------------
+-- @param: none
+-- @return: none
+function moonqueen:SkillBuildOverride()
+  moonqueen:SkillBuildOld()
+end
+moonqueen.SkillBuildOld = moonqueen.SkillBuild
+moonqueen.SkillBuild = moonqueen.SkillBuildOverride
+
+------------------------------------------------------
+--            onthink override                      --
+-- Called every bot tick, custom onthink code here  --
+------------------------------------------------------
+-- @param: tGameVariables
+-- @return: none
+function moonqueen:onthinkOverride(tGameVariables)
+  self:onthinkOld(tGameVariables)
+
+  -- custom code here
+end
+moonqueen.onthinkOld = moonqueen.onthink
+moonqueen.onthink = moonqueen.onthinkOverride
+
+----------------------------------------------
+--            oncombatevent override        --
+-- use to check for infilictors (fe. buffs) --
+----------------------------------------------
+-- @param: eventdata
+-- @return: none
+function moonqueen:oncombateventOverride(EventData)
+  self:oncombateventOld(EventData)
+
+  -- custom code here
+end
+-- override combat event trigger function.
+moonqueen.oncombateventOld = moonqueen.oncombatevent
+moonqueen.oncombatevent = moonqueen.oncombateventOverride
