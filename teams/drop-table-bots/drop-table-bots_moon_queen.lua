8083dff72111e55a8398e39d228544a89894f682
Niko Ilomäki <niko.ilomaki@helsinki.fi>
2013-05-07 19:26:00 +0300

    experimental routines for skillbuild and shop

--- teams/drop-table-bots/drop-table-bots_moon_queen.lua
+++ teams/drop-table-bots/drop-table-bots_moon_queen.lua
@@ -8,12 +8,32 @@ runfile 'bots/libhon/utils.lua'
 
 local core, behaviorLib = moonqueen.core, moonqueen.behaviorLib
 
+-- empty spaces filled with Minor Totems
+-- game adds Homecoming Stones to list at some point
+behaviorLib.StartingItems = { "6 Item_MinorTotem", "Item_Fleetfeet", "Item_Quickblade", "Item_Sicarius", "Item_BrainOfMaliken", "Item_ApprenticesRobe" }
+-- {Fleetfeet} -> {Fleetfeet, Quickblade} -> {Firebrand} -> {Firebrand, Pickled Brain} -> {Firebrand, Apprentice's Robe, Pickled Brain}
+behaviorLib.LaneItems = { "Item_ApprenticesRobe", "Item_NeophytesBook", "Item_Searinglight", "Item_Manatube" }
+-- {Firebrand, Apprentice's Robe, Neophyte's Book, Pickled Brain} -> {Searing Light, Pickled Brain} -> {Searing Light, Manatube, Pickled Brain}
+behaviorLib.MidItems = { "Item_BlessedArmband", "Item_MightyBlade", "Item_Dawnbringer", "Item_Regen", "Item_Confluence" }
+-- {Searing Light, Bolstering Armband, Manatube, Pickled Brain} -> {Searing Light, Bolstering Armband, Mighty Blade, Manatube, Pickled Brain}
+-- {Dawnbringer, Manatube, Pickled Brain} -> {Dawnbringer, Sustainer, Pickled Brain} -> {Dawnbringer, Sustainer, Blessed Orb, Pickled Brain}
+behaviorLib.LateItems = { "Item_Protect", "Item_Quickblade", "Item_Glowstone", "Item_MightyBlade", "Item_Intelligence7" }
+-- {Dawnbringer, Null Stone, Pickled Brain} -> {Dawnbringer, Null Stone, Quickblade, Pickled Brain} -> {Dawnbringer, Null Stone, Quickblade, Glowstone, Pickled Brain}
+-- {Dawnbringer, Null Stone, Quickblade, Mighty Blade, Glowstone, Pickled Brain} -> {Dawnbringer, Null Stone, Staff of the Master, Pickled Brain}
+
+behaviorLib.pushingStrUtilMul = 1
+
+moonqueen.skills = {}
+local skills = moonqueen.skills
+
+core.itemGeoBane = nil
+
 moonqueen.tSkills = {
-  1, 1, 1, 1, 1,
-  3, 0, 0, 0, 0,
-  3, 1, 1, 1, 2,
-  3, 2, 4, 4, 4,
-  4, 4, 4, 4, 4
+  1, 0, 0, 1, 4,
+  3, 0, 1, 1, 4,
+  3, 2, 2, 4, 4,
+  3, 4, 0, 0, 0,
+  2, 2, 4, 4, 4
 }
 
 ---------------------------------------------------------------
@@ -22,8 +42,17 @@ moonqueen.tSkills = {
 ---------------------------------------------------------------
 -- @param: none
 -- @return: none
+-- default
 function moonqueen:SkillBuildOverride()
-  moonqueen:SkillBuildOld()
+  local unitSelf = self.core.unitSelf
+  if skills.abilNuke == nil then
+    skills.abilNuke = unitSelf:GetAbility(0)
+    skills.abilBounce = unitSelf:GetAbility(1)
+    skills.abilAura = unitSelf:GetAbility(2)
+    skills.abilUltimate = unitSelf:GetAbility(3)
+    skills.stats = unitSelf:GetAbility(4)
+  end
+  self:SkillBuildOld()
 end
 moonqueen.SkillBuildOld = moonqueen.SkillBuild
 moonqueen.SkillBuild = moonqueen.SkillBuildOverride
