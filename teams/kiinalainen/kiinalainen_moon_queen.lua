b0559fec94c69b14ea9bc1a85024a7406feadfba
Hallo <ville.knuuttila@gmail.com>
2013-05-09 13:26:01 +0300

    our mq bot

--- teams/kiinalainen/kiinalainen_moon_queen.lua
+++ teams/kiinalainen/kiinalainen_moon_queen.lua
@@ -5,14 +5,38 @@ moonqueen.heroName = "Hero_Krixi"
 
 runfile 'bots/core_herobot.lua'
 
----------------------------------------------------------------
---            SkillBuild override                            --
--- Handles hero skill building. To customize just write own  --
----------------------------------------------------------------
--- @param: none
--- @return: none
+local core, behaviorLib = moonqueen.core, moonqueen.behaviorLib
+
+behaviorLib.StartingItems = { "Item_Punchdagger", "Item_HealthPotion"}
+behaviorLib.LaneItems = { "Item_EnhancedMarchers", "Item_HungrySpirit", "Item_WhisperingHelm" }
+behaviorLib.MidItems = { "Item_ManaBurn2", "Item_Evasion", "Item_Immunity", "Item_Stealth" }
+behaviorLib.LateItems = { "Item_LifeSteal4", "Item_Sasuke" }
+
+behaviorLib.pushingStrUtilMul = 1
+
+moonqueen.skills = {}
+local skills = moonqueen.skills
+
+core.itemGeoBane = nil
+
+moonqueen.tSkills = {
+  2, 1, 2, 0, 2,
+  3, 2, 0, 0, 0,
+  3, 1, 1, 1, 4,
+  3, 4, 4, 4, 4,
+  4, 4, 4, 4, 4
+}
+
 function moonqueen:SkillBuildOverride()
-  moonqueen:SkillBuildOld()
+  local unitSelf = self.core.unitSelf
+  if skills.abilNuke == nil then
+    skills.abilNuke = unitSelf:GetAbility(0)
+    skills.abilBounce = unitSelf:GetAbility(1)
+    skills.abilAura = unitSelf:GetAbility(2)
+    skills.abilUltimate = unitSelf:GetAbility(3)
+    skills.stats = unitSelf:GetAbility(4)
+  end
+  self:SkillBuildOld()
 end
 moonqueen.SkillBuildOld = moonqueen.SkillBuild
 moonqueen.SkillBuild = moonqueen.SkillBuildOverride
@@ -45,3 +69,117 @@ end
 -- override combat event trigger function.
 moonqueen.oncombateventOld = moonqueen.oncombatevent
 moonqueen.oncombatevent = moonqueen.oncombateventOverride
+
+local function NearbyCreepCount(botBrain, center, radius)
+  local count = 0
+  local unitsLocal = core.AssessLocalUnits(botBrain, center, radius)
+  local enemies = unitsLocal.EnemyCreeps
+  for _,unit in pairs(enemies) do
+    count = count + 1
+  end
+  return count
+end
+
+local function CustomHarassUtilityFnOverride(hero)
+  local nUtil = 0
+
+  if skills.abilNuke:CanActivate() then
+    nUtil = nUtil + 10
+  end
+
+  local creeps = NearbyCreepCount(moonqueen, hero:GetPosition(), 1000)
+
+  if skills.abilUltimate:CanActivate() and creeps < 2 then
+    nUtil = nUtil + 100
+  end
+
+  return nUtil
+end
+behaviorLib.CustomHarassUtility = CustomHarassUtilityFnOverride
+
+local function HarassHeroExecuteOverride(botBrain)
+
+  local unitTarget = behaviorLib.heroTarget
+  if unitTarget == nil then
+    return moonqueen.harassExecuteOld(botBrain)
+  end
+
+  local unitSelf = core.unitSelf
+  local nTargetDistanceSq = Vector3.Distance2DSq(unitSelf:GetPosition(), unitTarget:GetPosition())
+  local nLastHarassUtility = behaviorLib.lastHarassUtil
+
+  local bActionTaken = false
+
+  if core.CanSeeUnit(botBrain, unitTarget) then
+    local itemGeoBane = core.itemGeoBane
+    if not bActionTaken then
+      if itemGeoBane then
+        if itemGeoBane:CanActivate() then
+          bActionTaken = core.OrderItemClamp(botBrain, unitSelf, itemGeoBane)
+        end
+      end
+    end
+
+    local abilUltimate = skills.abilUltimate
+    if not bActionTaken and nLastHarassUtility > 50 then
+      if abilUltimate:CanActivate() then
+        local nRange = 600
+        if nTargetDistanceSq < (nRange * nRange) then
+          bActionTaken = core.OrderAbility(botBrain, abilUltimate)
+        else
+          bActionTaken = core.OrderMoveToUnitClamp(botBrain, unitSelf, unitTarget)
+        end
+      end
+    end
+
+    local abilNuke = skills.abilNuke
+    if abilNuke:CanActivate() then
+      local nRange = abilNuke:GetRange()
+      if nTargetDistanceSq < (nRange * nRange) then
+        bActionTaken = core.OrderAbilityEntity(botBrain, abilNuke, unitTarget)
+      else
+        bActionTaken = core.OrderMoveToUnitClamp(botBrain, unitSelf, unitTarget)
+      end
+    end
+  end
+
+  if not bActionTaken then
+    return moonqueen.harassExecuteOld(botBrain)
+  end
+end
+moonqueen.harassExecuteOld = behaviorLib.HarassHeroBehavior["Execute"]
+behaviorLib.HarassHeroBehavior["Execute"] = HarassHeroExecuteOverride
+
+local function DPSPushingUtilityOverride(myHero)
+  local modifier = 1 + myHero:GetAbility(1):GetLevel()*0.3
+  return moonqueen.DPSPushingUtilityOld(myHero) * modifier
+end
+moonqueen.DPSPushingUtilityOld = behaviorLib.DPSPushingUtility
+behaviorLib.DPSPushingUtility = DPSPushingUtilityOverride
+
+local function funcFindItemsOverride(botBrain)
+  local bUpdated = moonqueen.FindItemsOld(botBrain)
+
+  if core.itemGeoBane ~= nil and not core.itemGeoBane:IsValid() then
+    core.itemGeoBane = nil
+  end
+
+  if bUpdated then
+    if core.itemGeoBane then
+      return
+    end
+
+    local inventory = core.unitSelf:GetInventory(true)
+    for slot = 1, 12, 1 do
+      local curItem = inventory[slot]
+      if curItem then
+        if core.itemGeoBane == nil and curItem:GetName() == "Item_ManaBurn2" and not curItem:IsRecipe() then
+          core.itemGeoBane = core.WrapInTable(curItem)
+        end
+      end
+    end
+  end
+  return bUpdated
+end
+moonqueen.FindItemsOld = core.FindItems
+core.FindItems = funcFindItemsOverride
