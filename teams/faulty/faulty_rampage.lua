b174369cdd76426568f9d84c83e186ab74e9a3a5
Jesse Niemistö <jesse.niemi27@gmail.com>
2013-05-10 09:08:16 +0300

    rampage: Charge behavior.

--- teams/faulty/faulty_rampage.lua
+++ teams/faulty/faulty_rampage.lua
@@ -16,6 +16,10 @@ behaviorLib.LaneItems = { "Item_Warhammer", "Item_MightyBlade" }
 behaviorLib.MidItems = { "Item_Immunity", "Item_Steamboots" }
 behaviorLib.LateItems = { "Item_FrostfieldPlate" }
 
+local CHARGE_NONE, CHARGE_STARTED, CHARGE_TIMER, CHARGE_WARP = 0, 1, 2, 3
+rampage.charged = CHARGE_NONE
+
+
 -- http://www.gamereplays.org/heroesofnewerth/portals.php?show=page&name=Heroes-ofnewerth-Rampage-Guide&st=1
 -- Desired skill build
 -- 0 = Q(Stampede)
@@ -61,9 +65,9 @@ rampage.SkillBuild = rampage.SkillBuildOverride
 -- @param: tGameVariables
 -- @return: none
 function rampage:onthinkOverride(tGameVariables)
-  self:onthinkOld(tGameVariables)
+	self:onthinkOld(tGameVariables)
 
-  -- custom code here
+	-- custom code here
 end
 rampage.onthinkOld = rampage.onthink
 rampage.onthink = rampage.onthinkOverride
@@ -75,9 +79,23 @@ rampage.onthink = rampage.onthinkOverride
 -- @param: eventdata
 -- @return: none
 function rampage:oncombateventOverride(EventData)
-  self:oncombateventOld(EventData)
+	self:oncombateventOld(EventData)
 
-  -- custom code here
+	if EventData.Type == "Ability" and EventData.InflictorName == "Ability_Rampage1" then
+		self.charged = CHARGE_STARTED
+	elseif EventData.Type == "State_End" and EventData.StateName == "State_Rampage_Ability1_Timer" then
+		if self.charged == CHARGE_STARTED then
+			self.charged = CHARGE_NONE
+		end
+	elseif EventData.Type == "State" and EventData.StateName == "State_Rampage_Ability1_Warp" then
+		self.charged = CHARGE_WARP
+	elseif EventData.Type == "State_End" and EventData.StateName == "State_Rampage_Ability1_Warp" then
+		self.charged = CHARGE_NONE
+	elseif EventData.Type == "Death" then
+		self.charged = CHARGE_NONE
+	end
+
+	-- custom code here
 end
 rampage.oncombateventOld = rampage.oncombatevent
 rampage.oncombatevent = rampage.oncombateventOverride
@@ -131,6 +149,11 @@ local function CustomHarassUtilityFnOverride(hero)
 	rampage.doHarass = {} -- reset
 	local unitSelf = core.unitSelf
 
+	local heroPos = hero:GetPosition()
+	local selfPos = unitSelf:GetPosition()
+
+	local nTargetDistanceSq = Vector3.Distance2DSq(selfPos, heroPos)
+
 	local nRet = 0
 	local nMe = HeroStateValue(unitSelf, nEnemyNoMana, nEnemyNoHealth)
 	local nEnemy = HeroStateValue(hero, nEnemyNoMana, nEnemyNoHealth)
@@ -138,12 +161,14 @@ local function CustomHarassUtilityFnOverride(hero)
 
 	local bCanSee = core.CanSeeUnit(rampage, hero)
 
-	if skills.abilStampede:CanActivate() and bCanSee then
-		rampage.doHarass["target"] = hero
-		rampage.doHarass["skill"]  = skills.abilStampede
-		nRet = nRet + nStampede + skills.abilStampede:GetLevel() * nSkillLevelBonus
-
-		BotEcho(format("  CustomHarass, nRet: %g", nRet))
+	if skills.abilChains:CanActivate() and bCanSee then
+		local nRange = skills.abilChains:GetRange()
+		if nTargetDistanceSq < (nRange * nRange) then
+			rampage.doHarass["target"] = hero
+			rampage.doHarass["skill"]  = skills.abilChains
+			nRet = nRet + nStampede + skills.abilChains:GetLevel() * nSkillLevelBonus
+			BotEcho(format("  CustomHarass, Chains!: %g", nRet))
+		end
 	end
 
 	return nRet
@@ -166,6 +191,91 @@ local function HarassHeroExecuteOverride(botBrain)
 end
 rampage.harassExecuteOld = behaviorLib.HarassHeroBehavior["Execute"]
 behaviorLib.HarassHeroBehavior["Execute"] = HarassHeroExecuteOverride
+--------------------------------------------------------------------------------
+
+
+
+--------------------------------------------------------------------------------
+-- CHARGE BEHAVIOR
+--
+
+local ChargeBehavior = {}
+
+local function ChargeTarget(botBrain, unitSelf, abilCharge)
+	local tEnemyHeroes = HoN.GetHeroes(core.enemyTeam)
+	local utility = 0
+	local unitTarget = nil
+	local nTarget = 0
+	for nUID, unit in pairs(tEnemyHeroes) do
+		if core.CanSeeUnit(botBrain, unit) and unit:IsAlive() and (not unitTarget or unit:GetHealth() < unitTarget:GetHealth()) then
+			unitTarget = unit
+			nTarget = nUID
+		end
+	end
+	if unitTarget then
+		local damageLevels = {100,140,180,220}
+		local chargeDamage = damageLevels[abilCharge:GetLevel()]
+		local estimatedHP = unitTarget:GetHealth() - chargeDamage
+		if estimatedHP < 200 then
+			utility = 20
+		end
+		if unitTarget:GetManaPercent() < 30 then
+			utility = utility + 5
+		end
+		local level = unitTarget:GetLevel()
+		local ownLevel = unitSelf:GetLevel()
+		if level < ownLevel then
+			utility = utility + 5 * (ownLevel - level)
+		else
+			utility = utility - 10 * (ownLevel - level)
+		end
+		local vecTarget = unitTarget:GetPosition()
+		for nUID, unit in pairs(tEnemyHeroes) do
+			if nUID ~= nTarget and core.CanSeeUnit(botBrain, unit) and Vector3.Distance2DSq(vecTarget, unit:GetPosition()) < (500 * 500) then
+				utility = utility - 5
+			end
+		end
+	end
+	return unitTarget, utility
+end
+
+local function ChargeUtility(botBrain)
+	local abilStampede = skills.abilStampede
+	local unitSelf = core.unitSelf
+	if rampage.charged ~= CHARGE_NONE then
+		return 9999
+	end
+	if not abilStampede:CanActivate() then
+		return 0
+	end
+	local unitTarget, utility = ChargeTarget(botBrain, unitSelf, abilStampede)
+	if unitTarget then
+		rampage.chargeTarget = unitTarget
+		return utility
+	end
+	return 0
+end
+
+local function ChargeExecute(botBrain)
+	local bActionTaken = false
+	if botBrain.charged ~= CHARGE_NONE then
+		return true
+	end
+	if not rampage.chargeTarget then
+		return false
+	end
+	local abilStampede = skills.abilStampede
+	if abilStampede:CanActivate() then
+		BotEcho("ChargeExecute IMMA CHARGING!")
+		bActionTaken = core.OrderAbilityEntity(botBrain, abilStampede, rampage.chargeTarget)
+	end
+	return bActionTaken
+end
+
+ChargeBehavior["Utility"] = ChargeUtility
+ChargeBehavior["Execute"] = ChargeExecute
+ChargeBehavior["Name"] = "Charge like a boss"
+tinsert(behaviorLib.tBehaviors, ChargeBehavior)
 --------------------------------------------------------------------------------
 
 BotEcho("finished loading faulty_rampage.lua")
