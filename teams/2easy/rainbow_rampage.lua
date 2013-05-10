bf9f7e3795c68693c45ebee09fc25f7057540012
Jerry Mesimaki <jerry.mesimaki@cs.helsinki.fi>
2013-05-10 11:44:50 +0300

    ranpaaaaaake

--- teams/2easy/rainbow_rampage.lua
+++ teams/2easy/rainbow_rampage.lua
@@ -7,28 +7,24 @@ runfile 'bots/core_herobot.lua'
 runfile 'bots/lib/rune_controlling/init.lua'
 
 local core, behaviorLib = rampage.core, rampage.behaviorLib
-
 local tinsert = _G.table.insert
 
-behaviorLib.StartingItems = { "Item_RunesOfTheBlight", "Item_IronBuckler", "Item_LoggersHatchet" }
-behaviorLib.LaneItems = { "Item_Marchers", "Item_EnhancedMarchers", "Item_Pierce"}
-behaviorLib.MidItems = {  "Item_Shield2", "Item_PowerSupply", "Item_MysticVestments" }
-behaviorLib.LateItems = { "Item_Immunity", "Item_DaemonicBreastplate" }
-
-local CHARGE_NONE, CHARGE_STARTED, CHARGE_TIMER, CHARGE_WARP = 0, 1, 2, 3
-
-rampage.charged = CHARGE_NONE
+behaviorLib.StartingItems = { "Item_RunesOfTheBlight", "Item_LoggersHatchet", "Item_IronBuckler" }
+behaviorLib.LaneItems = { "Item_Marchers", "Item_EnhancedMarchers", "Item_Shield2"}
+behaviorLib.MidItems = {"Item_LifeSteal5"}
+behaviorLib.LateItems = {}
 
 rampage.skills = {}
 local skills = rampage.skills
 
 rampage.tSkills = {
-  1, 2, 1, 2, 1,
-  3, 2, 2, 1, 0,
+  2, 1, 2, 0, 2,
+  3, 2, 1, 1, 1,
   3, 0, 0, 0, 4,
   3, 4, 4, 4, 4,
   4, 4, 4, 4, 4
 }
+
 function rampage:SkillBuildOverride()
   local unitSelf = self.core.unitSelf
   if skills.abilCharge == nil then
@@ -66,82 +62,59 @@ rampage.onthink = rampage.onthinkOverride
 function rampage:oncombateventOverride(EventData)
   self:oncombateventOld(EventData)
 
-  if EventData.Type == "Ability" and EventData.InflictorName == "Ability_Rampage1" then
-    self.charged = CHARGE_STARTED
-  elseif EventData.Type == "State_End" and EventData.StateName == "State_Rampage_Ability1_Timer" then
-    if self.charged == CHARGE_STARTED then
-      self.charged = CHARGE_NONE
-    end
-  elseif EventData.Type == "State" and EventData.StateName == "State_Rampage_Ability1_Warp" then
-    self.charged = CHARGE_WARP
-  elseif EventData.Type == "State_End" and EventData.StateName == "State_Rampage_Ability1_Warp" then
-    self.charged = CHARGE_NONE
-  elseif EventData.Type == "Death" then
-    self.charged = CHARGE_NONE
-  end
+  -- custom code here
 end
 rampage.oncombateventOld = rampage.oncombatevent
 rampage.oncombatevent = rampage.oncombateventOverride
 
-
-
-local function KaiGetCreepAttackTargetOverride(botBrain, unitEnemyCreep, unitAllyCreep)
-    local unitSelf = core.unitSelf
-       
-        --Get closest hero. We can make decisions based on this.
-        local uClosestHero = nil
-        local nClosestHeroDistSq = 2000*2000 -- more than 2000 and we are not concerned. 2000*2000
-        for id, unitHero in pairs(HoN.GetHeroes(core.enemyTeam)) do
-                if unitHero ~= nil then
-                        if core.CanSeeUnit(botBrain, unitHero) and unitHero:GetTeam()~=team then
-                                local nDistanceSq = Vector3.Distance2DSq(unitHero:GetPosition(), core.unitSelf:GetPosition())
-                                if nDistanceSq < nClosestHeroDistSq then
-                                        nClosestHeroDistSq = nDistanceSq
-                                        uClosestHero = unitHero
-                                end
-                        end
-                end
-        end
-       
-        --addition is how much sooner to move towards creep. '30' is 30hp more than you can 1 hit kill it for.
-        addition=30
-        if (uClosestHero and uClosestHero:GetAttackType() ~= "melee") then --be more passive if opponent is ranged.
-                addition=20
-        end    
-       
-        if (uClosestHero and unitSelf:GetHealthPercent()*100<60 and Vector3.Distance2D(unitSelf:GetPosition(), uClosestHero:GetPosition())<uClosestHero:GetAttackRange()+150) then return nil end --too dangerous. Stay back.
-    local nDamageAverage = unitSelf:GetFinalAttackDamageMin()+addition --make the hero go to the unit when it is 40 hp away, but too risky if enemy is ranged
-        core.FindItems(botBrain)
-    if core.itemHatchet then
-        nDamageAverage = nDamageAverage * core.itemHatchet.creepDamageMul
-    end
-        if core.nDifficulty == core.nEASY_DIFFICULTY then
-                nDamageAverage = nDamageAverage + 10
-        end
-    if unitEnemyCreep and core.CanSeeUnit(botBrain, unitEnemyCreep) then --return enemy creep
-        local nTargetHealth = unitEnemyCreep:GetHealth()
-        if nDamageAverage >= nTargetHealth or core.teamBotBrain.nPushState==2 then
-            return unitEnemyCreep
-        end
-    end
-        if unitAllyCreep then
-                local nTargetHealth = unitAllyCreep:GetHealth()
-                if nDamageAverage >= nTargetHealth then
-                        local bActuallyDeny = true
-                        if core.teamBotBrain.nPushState~=2 and (core.nDifficulty ~= core.nEASY_DIFFICULTY or (core.bIsTutorial and core.bTutorialBehaviorReset == true and core.myTeam == HoN.GetHellbourneTeam())) then
-                                return unitAllyCreep
-                        end
-                end
-        end
-    return nil
+-- Harass Utility
+local function CustomHarassUtilityFnOverride(hero)
+  local nUtil = 15
+  if skills.abilBash:IsReady() then
+    nUtil = nUtil + 25
+  end
+  return nUtil
 end
-object.GetCreepAttackTargetOld = behaviorLib.GetCreepAttackTarget
-behaviorLib.GetCreepAttackTarget = KaiGetCreepAttackTargetOverride
+behaviorLib.CustomHarassUtility = CustomHarassUtilityFnOverride
+
+-- Harass Execute
+local function HarassHeroExecuteOverride(botBrain)
+  local abilCharge = skills.abilCharge
+  local abilUltimate = skills.abilUltimate
+  local abilSlow = skills.abilSlow
+
+  local unitTarget = behaviorLib.heroTarget
+  if unitTarget == nil then
+    return rampage.harassExecuteOld(botBrain)
+  end
+
+  local unitSelf = core.unitSelf
+  local nTargetDistanceSq = Vector3.Distance2DSq(unitSelf:GetPosition(), unitTarget:GetPosition())
+  local nLastHarassUtility = behaviorLib.lastHarassUtil
+
+  local bActionTaken = false
+
+  if core.CanSeeUnit(botBrain, unitTarget) then
+
+    if abilCharge:CanActivate() and unitTarget.GetHealthPercent() < 0.25 then
+      bActionTaken = core.OrderAbilityEntity(botBrain, abilCharge, unitTarget)
+    end
+
+  end
+
+  if not bActionTaken then
+    return rampage.harassExecuteOld(botBrain)
+  end
+end
+rampage.harassExecuteOld = behaviorLib.HarassHeroBehavior["Execute"]
+behaviorLib.HarassHeroBehavior["Execute"] = HarassHeroExecuteOverride
+
+-- Use hatchet
 
 local function advancedThinkUtility(botBrain)
         return 95; --always ridiculously important. Though, this rarly returns true.
 end
- 
+
 local function advancedThinkExecute(botBrain)
         local unitSelf = core.unitSelf
         local vecSelfPos = unitSelf:GetPosition()
@@ -169,80 +142,92 @@ behaviorLib.advancedThink["Execute"] = advancedThinkExecute
 behaviorLib.advancedThink["Name"] = "advancedThink"
 tinsert(behaviorLib.tBehaviors, behaviorLib.advancedThink)
 
-
-
-local function ChargeTarget(botBrain, unitSelf, abilCharge)
-  local tEnemyHeroes = HoN.GetHeroes(core.enemyTeam)
-  local utility = 0
-  local unitTarget = nil
-  local nTarget = 0
-  for nUID, unit in pairs(tEnemyHeroes) do
-    if core.CanSeeUnit(botBrain, unit) and unit:IsAlive() and (not unitTarget or unit:GetHealth() < unitTarget:GetHealth()) then
-      unitTarget = unit
-      nTarget = nUID
+-- Creep Attack Target Override
+--Kairus101's last hitter
+function behaviorLib.GetCreepAttackTarget(botBrain, unitEnemyCreep, unitAllyCreep) 
+--called pretty much constantly 
+   unitSelf=core.unitSelf
+    local bDebugEchos = false
+    -- predictive last hitting, don't just wait and react when they have 1 hit left (that would be stupid. T_T)
+ 
+ 
+    local unitSelf = core.unitSelf
+    local nDamageAverage = unitSelf:GetFinalAttackDamageMin()+40 --make the hero go to the unit when it is 40 hp away
+    core.FindItems(botBrain)
+    if core.itemHatchet then
+        nDamageAverage = nDamageAverage * core.itemHatchet.creepDamageMul
+    end   
+    -- [Difficulty: Easy] Make bots worse at last hitting
+    if core.nDifficulty == core.nEASY_DIFFICULTY then
+        nDamageAverage = nDamageAverage + 120
     end
-  end
-  if unitTarget then
-    local damageLevels = {100,140,180,220}
-    local chargeDamage = damageLevels[abilCharge:GetLevel()]
-    local estimatedHP = unitTarget:GetHealth() - chargeDamage
-    if estimatedHP < 200 then
-      utility = 20
+    if unitEnemyCreep and core.CanSeeUnit(botBrain, unitEnemyCreep) then
+        local nTargetHealth = unitEnemyCreep:GetHealth()
+        if nDamageAverage >= nTargetHealth then
+            local bActuallyLH = true
+            if bDebugEchos then BotEcho("Returning an enemy") end
+            return unitEnemyCreep
+        end
     end
-    if unitTarget:GetManaPercent() < 30 then
-      utility = utility + 5
+ 
+ 
+    if unitAllyCreep then
+        local nTargetHealth = unitAllyCreep:GetHealth()
+        if nDamageAverage >= nTargetHealth then
+            local bActuallyDeny = true
+ 
+ 
+            --[Difficulty: Easy] Don't deny
+            if core.nDifficulty == core.nEASY_DIFFICULTY then
+                bActuallyDeny = false
+            end           
+ 
+ 
+            -- [Tutorial] Hellbourne *will* deny creeps after **** gets real
+            if core.bIsTutorial and core.bTutorialBehaviorReset == true and core.myTeam == HoN.GetHellbourneTeam() then
+                bActuallyDeny = true
+            end
+ 
+ 
+            if bActuallyDeny then
+                if bDebugEchos then BotEcho("Returning an ally") end
+                return unitAllyCreep
+            end
+        end
     end
-    local level = unitTarget:GetLevel()
-    local ownLevel = unitSelf:GetLevel()
-    if level < ownLevel then
-      utility = utility + 5 * (ownLevel - level)
+    return nil
+end
+ 
+ 
+function KaiAttackCreepsExecuteOverride(botBrain)
+    local unitSelf = core.unitSelf
+    local currentTarget = core.unitCreepTarget
+ 
+ 
+    if currentTarget and core.CanSeeUnit(botBrain, currentTarget) then       
+        local vecTargetPos = currentTarget:GetPosition()
+        local nDistSq = Vector3.Distance2DSq(unitSelf:GetPosition(), vecTargetPos)
+        local nAttackRangeSq = core.GetAbsoluteAttackRangeToUnit(unitSelf, currentTarget, true)
+         
+        local nDamageAverage = unitSelf:GetFinalAttackDamageMin()if core.itemHatchet then
+nDamageAverage = nDamageAverage * core.itemHatchet.creepDamageMul
+end
+ 
+ 
+        if currentTarget ~= nil then
+            if nDistSq < nAttackRangeSq and unitSelf:IsAttackReady() and nDamageAverage>=currentTarget:GetHealth() then --only kill if you can get gold
+                --only attack when in nRange, so not to aggro towers/creeps until necessary, and move forward when attack is on cd
+                core.OrderAttackClamp(botBrain, unitSelf, currentTarget)
+            elseif (nDistSq > nAttackRangeSq) then
+                local vecDesiredPos = core.AdjustMovementForTowerLogic(vecTargetPos)
+                core.OrderMoveToPosClamp(botBrain, unitSelf, vecDesiredPos, false) --moves hero to target
+            else
+                core.OrderHoldClamp(botBrain, unitSelf, false) --this is where the magic happens. Wait for the kill.
+            end
+        end
     else
-      utility = utility - 10 * (ownLevel - level)
+        return false
     end
-    local vecTarget = unitTarget:GetPosition()
-    for nUID, unit in pairs(tEnemyHeroes) do
-      if nUID ~= nTarget and core.CanSeeUnit(botBrain, unit) and Vector3.Distance2DSq(vecTarget, unit:GetPosition()) < (500 * 500) then
-        utility = utility - 5
-      end
-    end
-  end
-  return unitTarget, utility
 end
-
-local function ChargeUtility(botBrain)
-  local abilCharge = skills.abilCharge
-  local unitSelf = core.unitSelf
-  if rampage.charged ~= CHARGE_NONE then
-    return 9999
-  end
-  if not abilCharge:CanActivate() then
-    return 0
-  end
-  local unitTarget, utility = ChargeTarget(botBrain, unitSelf, abilCharge)
-  if unitTarget then
-    rampage.chargeTarget = unitTarget
-    return utility
-  end
-  return 0
-end
-
-local function ChargeExecute(botBrain)
-  local bActionTaken = false
-  if botBrain.charged ~= CHARGE_NONE then
-    return true
-  end
-  if not rampage.chargeTarget then
-    return false
-  end
-  local abilCharge = skills.abilCharge
-  if abilCharge:CanActivate() then
-    bActionTaken = core.OrderAbilityEntity(botBrain, abilCharge, rampage.chargeTarget)
-  end
-  return bActionTaken
-end
-
-local ChargeBehavior = {}
-ChargeBehavior["Utility"] = ChargeUtility
-ChargeBehavior["Execute"] = ChargeExecute
-ChargeBehavior["Name"] = "Charge like a boss"
-tinsert(behaviorLib.tBehaviors, ChargeBehavior)
+object.AttackCreepsExecuteOld = behaviorLib.HarassHeroBehavior["Execute"]
+behaviorLib.AttackCreepsBehavior["Execute"] = KaiAttackCreepsExecuteOverride
