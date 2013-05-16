a9aec638e58440d666b8443876278806b7635c0d
Jesse Niemistö <jesse.niemi27@gmail.com>
2013-05-16 16:40:45 +0300

    magmus: minor changes.

--- teams/faulty/faulty_magmus.lua
+++ teams/faulty/faulty_magmus.lua
@@ -2,15 +2,6 @@ local _G = getfenv(0)
 local magmus = _G.object
 
 runfile 'bots/magmus/magmus_main.lua'
-runfile 'bots/teams/default/utils/sitter.lua'
+runfile 'bots/lib/rune_controlling/init.lua'
 
 local core, behaviorLib = magmus.core, magmus.behaviorLib
-
-local function PreGameExecuteOverride(botBrain)
-  local unitSelf = core.unitSelf
-  if not unitSelf.isSitter then
-    return behaviorLib.PreGameExecute(botBrain)
-  end
-  return behaviorLib.PreGameSitterExecute(botBrain)
-end
-behaviorLib.PreGameBehavior["Execute"] = PreGameExecuteOverride
