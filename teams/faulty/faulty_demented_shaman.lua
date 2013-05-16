8f6ae1904b6adde7dc7f0306090e3ece01ce00f3
Jesse Niemistö <jesse.niemi27@gmail.com>
2013-05-16 21:39:36 +0300

    DS and hammer, bit less aggressive around enemy towers.

--- teams/faulty/faulty_demented_shaman.lua
+++ teams/faulty/faulty_demented_shaman.lua
@@ -3,6 +3,7 @@ local shaman = _G.object
 
 runfile 'bots/dementedshaman/dementedshaman_main.lua'
 runfile 'bots/teams/default/utils/sitter.lua'
+runfile 'bots/teams/faulty/utils.lua'
 
 local core, behaviorLib = shaman.core, shaman.behaviorLib
 
@@ -14,3 +15,26 @@ local function PreGameExecuteOverride(botBrain)
   return behaviorLib.PreGameSitterExecute(botBrain)
 end
 behaviorLib.PreGameBehavior["Execute"] = PreGameExecuteOverride
+
+--------------------------------------------------------------------------------
+-- CUSTOM HARASS UTILITY to default custom harass utility
+--
+-- Don't be nearly as agressive when close to enemy towers
+
+local nNoHealth = 20
+local nNoMana   = 10
+
+local function CustomHarassUtilityOverride2(hero)
+	local nUtility = shaman.CustomHarassUtilityOld(hero)
+
+	nUtility = nUtility + HeroStateValueUtility(hero, nNoMana, nNoHealth)
+	nUtility = nUtility - HeroStateValueUtility(core.unitSelf, nNoMana, nNoHealth)
+
+	if core.GetClosestEnemyTower(core.unitSelf:GetPosition(), 715) then
+		nUtility = nUtility / 2
+	end
+
+	return nUtility
+end
+shaman.CustomHarassUtilityOld = behaviorLib.CustomHarassUtility
+behaviorLib.CustomHarassUtility = CustomHarassUtilityOverride2
